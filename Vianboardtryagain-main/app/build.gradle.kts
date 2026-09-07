b21415204b6048b4b4049178bc961a6759fdaae4
schuylervianilewis-hash <schuylervianilewis@gmail.com>
2026-09-08 01:42:23 +0530

    [6/11] Deploy remix-remix-vian-board via Vian Github Help

---
+++ Vianboardtryagain-main/app/build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.compose)
+  alias(libs.plugins.kotlin.serialization)
+  alias(libs.plugins.secrets)
+}
+
+android {
+  namespace = "helium314.keyboard.latin"
+  compileSdk { version = release(36) { minorApiLevel = 1 } }
+
+  defaultConfig {
+    applicationId = "shura.vianboard"
+    minSdk = 24
+    targetSdk = 36
+    versionCode = 4100
+    versionName = "4.1-beta1"
+
+    resourceConfigurations += listOf("en", "fr")
+    ndk {
+      abiFilters.addAll(listOf("arm64-v8a", "x86_64"))
+    }
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  signingConfigs {
+    create("debugConfig") {
+      storeFile = file("${rootDir}/debug.keystore")
+      storePassword = "android"
+      keyAlias = "androiddebugkey"
+      keyPassword = "android"
+    }
+  }
+
+  buildTypes {
+    release {
+      isCrunchPngs = false
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+    debug { signingConfig = signingConfigs.getByName("debugConfig") }
+  }
+  compileOptions {
+    isCoreLibraryDesugaringEnabled = true
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  buildFeatures {
+    viewBinding = true
+    compose = true
+    buildConfig = true
+  }
+  testOptions { unitTests { isIncludeAndroidResources = true } }
+  dependenciesInfo {
+    includeInApk = false
+    includeInBundle = false
+  }
+}
+
+secrets {
+  propertiesFileName = ".env"
+  defaultPropertiesFileName = ".env.example"
+}
+
+dependencies {
+  coreLibraryDesugaring(libs.desugar.jdk.libs)
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.recyclerview)
+  implementation(libs.androidx.autofill)
+  implementation(libs.androidx.viewpager2)
+  implementation(libs.kotlinx.serialization.json)
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.compose.material3)
+  implementation(libs.androidx.compose.ui)
+  implementation(libs.androidx.compose.ui.graphics)
+  implementation(libs.androidx.compose.ui.tooling.preview)
+  implementation(libs.androidx.navigation.compose)
+  implementation(libs.reorderable)
+  implementation(libs.colorpicker.compose)
+  debugImplementation(libs.androidx.compose.ui.tooling)
+  debugImplementation(libs.androidx.compose.ui.test.manifest)
+}
