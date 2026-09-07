59712443065fce53f9d805e206a97ec0cad29b03
schuylervianilewis-hash <schuylervianilewis@gmail.com>
2026-09-08 01:42:02 +0530

    [5/11] Deploy remix-remix-vian-board via Vian Github Help

---
+++ Vianboardtryagain-main/app/src/main/java/helium314/keyboard/keyboard/internal/LayoutDirective.java
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: GPL-3.0-only
+package helium314.keyboard.keyboard.internal;
+
+import androidx.annotation.Nullable;
+
+import helium314.keyboard.latin.utils.CapsModeUtils;
+import helium314.keyboard.latin.utils.RecapitalizeMode;
+
+public sealed interface LayoutDirective {
+    KeyboardState.Mode mode();
+
+    record Alphabet(
+        ShiftMode shiftMode,
+        int autoCapsFlags,
+        @Nullable RecapitalizeMode recapitalizeMode
+    ) implements LayoutDirective {
+        @Override
+        public KeyboardState.Mode mode() {
+            return KeyboardState.Mode.ALPHABET;
+        }
+
+        @Override
+        public String toString() {
+            return "Alphabet{shiftMode=" + shiftMode
+                 + ", autoCapsFlags=" + CapsModeUtils.flagsToString(autoCapsFlags)
+                 + ", recapitalizeMode=" + recapitalizeMode
+                 + '}'
+            ;
+        }
+    }
+
+    enum Utility implements LayoutDirective {
+        SYMBOLS(KeyboardState.Mode.SYMBOLS),
+        SYMBOLS_SHIFTED(KeyboardState.Mode.SYMBOLS_SHIFTED),
+        EMOJI(KeyboardState.Mode.EMOJI),
+        CLIPBOARD(KeyboardState.Mode.CLIPBOARD),
+        NUMPAD(KeyboardState.Mode.NUMPAD),
+        DPAD(KeyboardState.Mode.DPAD),
+    ;
+        private final KeyboardState.Mode mMode;
+
+        Utility(KeyboardState.Mode mode) {
+            mMode = mode;
+        }
+
+        @Override
+        public KeyboardState.Mode mode() {
+            return mMode;
+        }
+    }
+}
