4c1765d313f85732e6c2a990f9a9f41754e5fc07
schuylervianilewis-hash <schuylervianilewis@gmail.com>
2026-09-09 02:08:27 +0530

    [14/22] Deploy vianboard via Vian Github Help

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,9 +19,10 @@ dependencyResolutionManagement {
   repositories {
     google()
     mavenCentral()
+    maven { url = uri("https://jitpack.io") }
   }
 }
 
-rootProject.name = "Vian Board"
+rootProject.name = "VianBoard"
 
 include(":app")
